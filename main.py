3d0c6c5501409399b2b38b8e1a10cf69a3758868
Madhav Gupta <e22cseu0827@bennett.edu.in>
2024-08-02 00:38:39 +0530

    render test

--- main.py
+++ main.py
@@ -1,11 +1,65 @@
 from xls2xlsx import XLS2XLSX
-from flask import Flask, jsonify, request
-x2x = XLS2XLSX("spreadsheet.xls")
-x2x.to_xlsx("spreadsheet.xlsx")
+from flask import Flask, jsonify, request, send_file
+from werkzeug.utils import secure_filename
+from flask_cors import CORS
+from io import BytesIO
+from openpyxl import Workbook
+import os
+
 
 
 
 app = Flask(__name__) 
+
+
+ALLOWED_EXTENSIONS = set(['xls', 'csv', 'png', 'jpeg', 'jpg'])
+UPLOAD_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), 'Downloads'))
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 500 * 1000 * 1000  # 500 MB
+app.config['CORS_HEADER'] = 'application/json'
+
+def allowedFile(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           
+           
+def download_excel(dir):
+    x2x = XLS2XLSX()
+    wb = x2x.to_xlsx()
+
+    file_stream = BytesIO()
+    wb.save(file_stream)
+    file_stream.seek(0)
+
+    return send_file(file_stream, as_attachment=True, download_name="excel.xlsx")
+           
+           
+@app.route('/upload', methods=['POST', 'GET'])
+def fileUpload():
+    if request.method == 'POST':
+        file = request.files.getlist('files')
+        filename = ""
+        print(request.files, "....")
+        for f in file:
+            print(f.filename)
+            filename = secure_filename(f.filename)
+            print(allowedFile(filename))
+            if allowedFile(filename):
+                f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            else:
+                return jsonify({'message': 'File type not allowed'}), 400
+        #download_excel(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        x2x = XLS2XLSX(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        wb = x2x.to_xlsx()
+
+        file_stream = BytesIO()
+        wb.save(file_stream)
+        file_stream.seek(0)
+
+        return send_file(file_stream, as_attachment=True, download_name="excel.xlsx")
+        # return jsonify({"name": filename, "status": "success"})
+    else:
+        return jsonify({"status": "Upload API GET Request Running"})
 
 
 # on the terminal type: curl http://127.0.0.1:5000/ 
